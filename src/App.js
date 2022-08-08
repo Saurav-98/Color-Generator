54c1c2065eb6c5a3b3b0735fee51e3ec4fcac471
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-08-08 18:24:43 +0530

    Color functionality added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
 
-import Values from 'values.js'
+import Values from 'values.js';
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState([]);
+  const [error, setError] = useState(false);
+
+  const [randomList, setRandomList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    try {
+      setError(false);
+      let colors = new Values(color).all(10);
+      setRandomList(colors);
+    } catch (error) {
+      console.log('Error');
+      setError(true);
+    }
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>Color Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            className={error ? 'error' : ''}
+            placeholder="#f15025"
+            onChange={(e) => setColor(e.target.value)}
+          />
+          <button type="submit" className="btn">
+            Submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {randomList.map((color, index) => {
+          console.log(color);
+          return (
+            <SingleColor
+              hexColor={color.hex}
+              key={index}
+              {...color}
+              index={index}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,8 +1,22 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from 'react';
+import rgbToHex from './utils';
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const hex = rgbToHex(...rgb);
 
-export default SingleColor
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(',');
+  console.log(hexColor);
+  const hexVal = `#${hexColor}`;
+  return (
+    <article
+      className={`color ${index > 10 && 'color-light'}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexVal}</p>
+    </article>
+  );
+};
+
+export default SingleColor;
